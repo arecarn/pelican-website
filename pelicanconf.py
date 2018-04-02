b2e1c08aac92deb97d83ca1aaa8d94472d9a8ad8
Ryan Carney <arecarn@gmail.com>
2018-04-01 22:20:23 -0700

    Remove unused settings in pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,16 +35,6 @@ DEFAULT_METADATA = {
         'status': 'draft',
 }
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 PLUGIN_PATHS = ['./build/pelican-plugins']
 PLUGINS = ['pelican_javascript']
 
